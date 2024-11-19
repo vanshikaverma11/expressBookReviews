ca3018cd5ec542ee7cd8b60f3f25e29547b4f8e5
vanshikaverma11 <vanshikav4567@gmail.com>
2024-11-19 17:34:43 +0530

    updateall

--- final_project/index.js
+++ final_project/index.js
@@ -11,7 +11,20 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+//authenication mechanism here
+const token = req.session.authorization ? req.session.authorization.accessToken : null;
+
+if (!token) {
+  return res.status(403).json({ message: "User not authenticated. Please login first." });
+}
+
+jwt.verify(token, "access", (err, decoded) => {
+  if (err) {
+    return res.status(403).json({ message: "Invalid or expired token." });
+  }
+  req.user = decoded; // Store the decoded token data (user info)
+  next(); // Proceed to the next middleware or route
+});
 });
  
 const PORT =5000;
