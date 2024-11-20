f5542986d89612330224f89a0549fd2c483cc125
vanshikaverma11 <vanshikav4567@gmail.com>
2024-11-20 10:48:02 +0530

    updaet

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -19,14 +19,15 @@ public_users.get('/', function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn', function (req, res) {
-  const isbn = req.params.isbn;
+  const isbn = req.params.isbn; // Extract ISBN from the request parameter
 
   if (books[isbn]) {
-    res.status(200).json(books[isbn]);
+    res.status(200).json(books[isbn]); // Correct status code
   } else {
-    res.status(404).json({ message: "Book not found" });
+    res.status(404).json({ message: "Book not found" }); // Error message if ISBN not found
   }
 });
+
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
